e43bc0041a8982b2bcfc43b84d742882a5f409c4
unknown <damien.gaillard@ext.imprimerienationale.fr>
2018-09-25 11:10:25 +0200

    design: add animations

--- core/src/fr/clodo/arena/drawables/Dwarf.kt
+++ core/src/fr/clodo/arena/drawables/Dwarf.kt
@@ -8,15 +8,15 @@ import fr.clodo.arena.helper.Animator
 import fr.clodo.arena.helper.ClodoWorld
 import fr.clodo.arena.screens.GameScreen
 
-class Dwarf(val walkingAnimation: Animation<TextureRegion>) : Drawable() {
+class Dwarf(val walkingAnimation: Animation<TextureRegion>, val idleingAnimation: Animation<TextureRegion>, val attackingAnimation: Animation<TextureRegion>) : Drawable() {
 
     companion object {
         const val TAG = "Dwarf"
         fun createDwarf(): Dwarf {
-            return Dwarf(getWalkAnimation())
+            return Dwarf(getWalkAnimation(), getIdleAnimation(), getAttackAnimation())
         }
 
-        private const val frameDuration = 0.25f
+        private const val frameDuration = 0.15f
         private const val sizeX = 64f
         private const val sizeY = 64f
         private const val startX = 20f
@@ -24,30 +24,66 @@ class Dwarf(val walkingAnimation: Animation<TextureRegion>) : Drawable() {
         const val speedX = 300f
 
         private fun getWalkAnimation() = Animation(frameDuration, getWalkTexture(), Animation.PlayMode.LOOP)
-        private fun getWalkTexture() = Animator.generateArray("Nain.png", 0, 0, 0, 11, 128, 128, 1408, 128)
+        private fun getWalkTexture() = Animator.generateArray("spritesheet.png", 0, 2, 0, 1, 68, 67, 202, 198)
+        //private fun getWalkTexture() = Animator.generateArray("Nain.png", 0, 0, 0, 11, 128, 128, 1408, 128)
+
+        private fun getIdleAnimation() = Animation(frameDuration, getIdleTexture(), Animation.PlayMode.LOOP)
+        private fun getIdleTexture() = Animator.generateArray("spritesheet_idle.png", 0, 0, 0, 4, 50, 72, 202, 72)
+
+        private fun getAttackAnimation() = Animation(frameDuration, getAttackTexture(), Animation.PlayMode.LOOP)
+        private fun getAttackTexture() = Animator.generateArray("spritesheet_attack.png", 0, 0, 0, 4, 100, 62, 400, 62)
     }
 
     private var stateTime: Float = 0f
 
     init {
         sprite.setPosition(startX, startY)
-        sprite.setSize(sizeX , sizeY )
+        sprite.setSize(sizeX, sizeY)
     }
 
     override fun update(delta: Float) {
         stateTime += delta
-        when(ClodoWorld.currentScreen){
+        when (ClodoWorld.currentScreen) {
+            ClodoScreen.LOBBY -> {
+            }
+            ClodoScreen.IN_GAME_WAITING -> {
+            }
             ClodoScreen.IN_GAME_WALKING -> {
                 val fl = speedX * delta
                 sprite.setPosition(sprite.x + fl, sprite.y)
 //                Gdx.app.log(TAG, "Dwarf avance a la vitesse de $speedX et un delta $delta pour un total de $fl")
 //                Gdx.app.log(TAG, "Position = ${sprite.x + fl}")
             }
+            ClodoScreen.IN_GAME_FIGHTING -> {
+            }
+            ClodoScreen.DEAD -> {
+            }
         }
     }
 
     override fun draw(batch: SpriteBatch, font: BitmapFont, delta: Float) {
-        sprite.setRegion(walkingAnimation.getKeyFrame(stateTime))
+        when (ClodoWorld.currentScreen) {
+
+            ClodoScreen.LOBBY -> {
+                //TODO
+                sprite.setRegion(idleingAnimation.getKeyFrame(stateTime))
+            }
+            ClodoScreen.IN_GAME_WAITING -> {
+                sprite.setRegion(idleingAnimation.getKeyFrame(stateTime))
+            }
+            ClodoScreen.IN_GAME_WALKING -> {
+                sprite.setRegion(walkingAnimation.getKeyFrame(stateTime))
+            }
+            ClodoScreen.IN_GAME_FIGHTING -> {
+                //TODO
+                sprite.setRegion(idleingAnimation.getKeyFrame(stateTime))
+            }
+            ClodoScreen.DEAD -> {
+                //TODO
+                sprite.setRegion(idleingAnimation.getKeyFrame(stateTime))
+            }
+        }
+
         sprite.draw(batch)
     }
 
--- core/src/fr/clodo/arena/helper/Animator.kt
+++ core/src/fr/clodo/arena/helper/Animator.kt
@@ -5,35 +5,38 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.utils.Array
 
-abstract class Animator{
+abstract class Animator {
     companion object {
         const val TAG = "Animator"
 
         fun generateArray(spriteSheet: String, startRow: Int, endRow: Int, startCol: Int, endCol: Int, frameX: Int, frameY: Int, width: Int, height: Int): Array<TextureRegion> {
 
-            val nbCol = width / frameX
-            val nbRow = height / frameY
             val tmp = TextureRegion.split(Texture(spriteSheet), frameX, frameY)
 
 
-            var currentRow = startRow
-            var currentCol = startCol
+            var currentRow = startRow //3
+            var currentCol = startCol //2
             var list = Array<TextureRegion>()
-
-            while (currentCol < endCol || currentRow < endRow) {
-                list.add(tmp[currentRow][currentCol])
+            //  4 1
+            while (currentRow < endRow || currentCol < endCol) {
+                try {
+                    list.add(tmp[currentRow][currentCol])
+                } catch (e: Exception) {
+                    var i = 2
+                }
                 currentCol++
-                if (currentCol > nbCol) {
+                if (currentRow == endRow && currentCol == endCol) {
+                    Gdx.app.error(TAG, "Done")
+                    return list
+                }
+                if (currentCol >= tmp[currentRow].size) {
                     currentCol = 0
                     currentRow++
-                }
-                if(currentRow > nbRow){
-                    Gdx.app.error(TAG, "SHIT HAPPENS")
-                    break
                 }
             }
 
             return list
+
         }
     }
 }
